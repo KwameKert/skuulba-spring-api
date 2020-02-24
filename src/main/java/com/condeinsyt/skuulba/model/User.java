5051225e58613c3315f5d2f93f4c717b2f917239
kwamekert <tomriddle477@gmail.com>
2020-02-24 14:42:02 +0000

    CRUD service completed

--- src/main/java/com/condeinsyt/skuulba/model/User.java
+++ src/main/java/com/condeinsyt/skuulba/model/User.java
@@ -1,11 +1,98 @@
 package com.condeinsyt.skuulba.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="app_user")
 public class User {
-	
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userName;
+    private String password;
+    private String role;
+    private Integer status;
+
+
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    public User() {
+    }
+
+
+    public User(String userName, String password, String role, Integer status, Date createdAt) {
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                ", status=" + status +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
---
+++ src/main/java/com/condeinsyt/skuulba/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.condeinsyt.skuulba.repository;
+
+import com.condeinsyt.skuulba.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, Long> {
+
+     User findByUsername(String userName);
+}
