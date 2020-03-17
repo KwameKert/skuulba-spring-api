163bf09c70faa33a919d86c073005bd0e5e4339a
kwamekert <tomriddle477@gmail.com>
2020-03-17 12:04:00 +0000

    InvoiceFee & invoiceItem repository created

---
+++ src/main/java/com/condeinsyt/skuulba/repository/InvoiceFeeRepository.java
@@ -0,0 +1,8 @@
+package com.condeinsyt.skuulba.repository;
+
+
+import com.condeinsyt.skuulba.model.InvoiceFee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceFeeRepository extends JpaRepository<InvoiceFee, Long> {
+}
